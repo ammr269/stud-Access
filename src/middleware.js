887a55803970798e8ae639e826a20cb926868354
Faouzi Mohamed <me@mfaouzi.com>
2025-01-30 00:41:25 +0100

    fix: middleware file

--- src/middleware.js
+++ src/middleware.js
@@ -16,15 +16,6 @@ export const config = {
 // do not allow js, css, map files, and other assets to be prefetched
 const matcherRegex = new RegExp('^(?!/.*(?:map|legal-assets|js|icon|robot)).*');
 
-const corsOptions = {
-  'Access-Control-Allow-Credentials': 'true',
-  'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS',
-  'Access-Control-Allow-Headers':
-    'Accept,Authorization,Cache-Control,Content-Type,DNT,Expires,If-Modified-Since,Pragma,Range,User-Agent,X-Requested-With',
-  'Access-Control-Expose-Headers':
-    'Cache-Control,Content-Language,Content-Length,Content-Type,Content-Range,Expires,Last-Modified,Pragma',
-};
-
 export default withAuth(middleware, {
   callbacks: {
     authorized: () => {
@@ -36,26 +27,12 @@ export default withAuth(middleware, {
 async function middleware(req) {
   // const reqLogger = new RequestLogger(logger, req);
   const isMiddlewareAllowed = matcherRegex.test(req.nextUrl.pathname);
-
   if (!isMiddlewareAllowed) {
     return NextResponse.next();
   }
 
-  // TODO: Check the origin from the request
-
-  // Handle preflighted requests
-  const isPreflight = req.method === 'OPTIONS';
-  if (isPreflight) {
-    const preflightHeaders = {
-      'Access-Control-Max-Age': '1728000',
-      'Content-Type': 'text/plain; charset=utf-8',
-      'Content-Length': '0',
-      ...corsOptions,
-    };
-    return NextResponse.json({}, { headers: preflightHeaders });
-  }
-
   if (req.nextUrl.pathname.startsWith('/api')) {
+    console.log(`  => API: ${req.method.toUpperCase()} `, req.nextUrl.pathname);
     return NextResponse.next();
   }
 
@@ -65,23 +42,18 @@ async function middleware(req) {
   if (shouldSigning) return shouldSigning();
 
   // redirect to the home page if the user is logged in and tries to access the login/signup page
-  if (IsAuthPageAndisLoggedIn(req, token)) {
-    console.log('  =>you are allready connected:', req.nextUrl.pathname);
+  if (IsAuthPageAndIsLoggedIn(req, token)) {
+    console.log('  => You are already connected: ', req.nextUrl.pathname);
     return NextResponse.redirect(new URL(`/`, req.url));
   }
 
   // Handle simple requests
-  const response = NextResponse.next();
-  for (const [key, value] of Object.entries(corsOptions)) {
-    response.headers.set(key, value);
-  }
-  return response;
+  return NextResponse.next();
 }
 
 export function forceToLoginOnProtectedRoutes(req, token) {
-  const now = new Date();
   if (
-    (!token || new Date(token.expiry).getTime() < now.getTime()) &&
+    !token &&
     !['/', '/authentification', '/inscription'].includes(req.nextUrl.pathname)
   ) {
     console.log(`   => Path '${req.nextUrl.pathname}' is protected`);
@@ -97,7 +69,7 @@ export function forceToLoginOnProtectedRoutes(req, token) {
   }
 }
 
-function IsAuthPageAndisLoggedIn(req, token) {
+function IsAuthPageAndIsLoggedIn(req, token) {
   return (
     (req.nextUrl.pathname.startsWith(`/authentification`) ||
       req.nextUrl.pathname.startsWith(`/inscription`)) &&
