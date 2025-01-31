6f0615d13b7b39364f7cbcdce48a0b3e3eb31d72
Ammar <ammarmed333@gmail.com>
2025-01-31 11:13:00 +0100

    update Lock page

--- src/components/packages/providers/app-session-provider.jsx
+++ src/components/packages/providers/app-session-provider.jsx
@@ -1,24 +1,45 @@
 'use client';
-import { AbsoluteCenter, Button, Icon, Stack, Text } from '@chakra-ui/react';
+import {
+  AbsoluteCenter,
+  Button,
+  ButtonGroup,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import Logo from '~/images/logost.png';
+import NextImage from 'next/image';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
-import { FaLock } from 'react-icons/fa';
+// import { FaLock } from 'react-icons/fa';
 
 export default function AppSessionProvider({ children }) {
   const { status } = useSession();
 
   if (status === 'unauthenticated')
     return (
-      <Stack position='absolute' inset={0} bg='red.50'>
+      <Stack position='absolute' inset={0} bg='white'>
         <AbsoluteCenter>
           <Stack alignItems='center'>
-            <Icon fontSize='4rem' asChild color='red.700'>
+            {/* <Icon fontSize='4rem' asChild color='red.700'>
               <FaLock />
-            </Icon>
-            <Text fontWeight='600'>Vous n&apos;êtes pas connecté.</Text>
-            <Button h='auto' colorPalette='red' variant='ghost' asChild>
-              <Link href='/authentification'>Connexion</Link>
-            </Button>
+            </Icon> */}
+            <NextImage
+              src={Logo}
+              alt='Stud Access Logo'
+              width={300}
+              height={300}
+            />
+            <Text fontWeight='600'>
+              Vous devez vous connecté pour commencer.
+            </Text>
+            <ButtonGroup>
+              <Button h='auto' colorPalette='blue' asChild>
+                <Link href='/authentification'>Connexion</Link>
+              </Button>
+              <Button h='auto' colorPalette='green' asChild>
+                <Link href='/inscription'>S&apos;inscrire</Link>
+              </Button>
+            </ButtonGroup>
           </Stack>
         </AbsoluteCenter>
       </Stack>
